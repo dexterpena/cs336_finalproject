7b493054ded3c74098e96c98532a726b452ef4df
Dexter Pena <65513325+dexter-pena@users.noreply.github.com>
2024-12-08 17:53:56 -0500

    Remove unused files and create menu for filters.

--- src/main/java/com/rudbmsgroupproject/project_2/repository/MsamdRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rudbmsgroupproject.project_2.repository;
-
-import com.rudbmsgroupproject.project_2.model.Msamd;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MsamdRepository extends JpaRepository<Msamd, Integer> {
-}
